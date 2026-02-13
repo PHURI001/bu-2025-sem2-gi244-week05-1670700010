679f299d8e541c0a394eb1cb90d690a693a8c7ee
PHURI001 <phuriwat.chat@gmail.com>
2026-02-13 10:27:41 +0700

    work in progress

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public int attackPoint = 5;
+    public int attackPoint = 50;
 
     void OnTriggerEnter(Collider other)
     {
-
+        Debug.Log("Hit " + other.gameObject.name);
+        var health = other.gameObject.GetComponent<HealthV1>();
+        health.TakeDamage(attackPoint);
     }
 }
--- Assets/Scripts/WaveSpawnManager.cs
+++ Assets/Scripts/WaveSpawnManager.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class WaveSpawnManager : MonoBehaviour
 {
+    public Wave[] waveConfigs;
+    public WaveController waveController;
+
+    private int currentWaveIdex = 0;
+    private float waveEndTime = 0;
+
     void Start()
     {
 
